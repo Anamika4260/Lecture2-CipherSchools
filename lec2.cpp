47e463cd978d50eebad9585c186cb6619393a67e
Anamika4260 <104004726+Anamika4260@users.noreply.github.com>
2023-07-04 06:12:55 +0530

    Add files via upload

---
+++ lec2.cpp
@@ -0,0 +1,10 @@
+FIRST PROGRAM " HELLO WORLD"
+
+#include<iostream> // this is header file
+using namespace std; // std makes sure that cout is a part of function 
+int main() // we have written a main "function"
+{
+cout<<"hello world"; // this will printout hello world on the user screen
+
+return 0;
+}
